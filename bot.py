ac35c784963975963d2873512bc213c410ab0f46
Cristian Maureira-Fredes <Cristian.Maureira-Fredes@qt.io>
2020-10-05 15:36:57 +0200

    Update welcome message

--- bot.py
+++ bot.py
@@ -179,9 +179,10 @@ def welcome(update, context):
         wiki_url = "https://pyside.org"
         msg = (
             f"Welcome {new_user} {party} {confetti}\n"
-            f"Remember to check the documentation page {doc_url}\n"
-            f"and the Wiki: {wiki_url}\n"
-            f"if you have some questions\n"
+            f"Verify you are not a bot solving the captcha!\n\n"
+            f"Remember to check the documentation page {doc_url} "
+            f"and the wiki {wiki_url} "
+            f"if you have some questions.\n"
         )
 
         context.bot.send_message(chat_id=chat_id, text=msg, parse_mode="HTML")
